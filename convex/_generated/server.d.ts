94e703ebd78832c17a37be2f49842e5c9e42cb4b
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2026-02-05 12:09:53 -0600

    Eliminar comentarios de generación automática en archivos de API y modelo de datos

--- convex/_generated/server.d.ts
+++ convex/_generated/server.d.ts
@@ -1,13 +1,3 @@
-/* eslint-disable */
-/**
- * Generated utilities for implementing server-side Convex query and mutation functions.
- *
- * THIS CODE IS AUTOMATICALLY GENERATED.
- *
- * To regenerate, run `npx convex dev`.
- * @module
- */
-
 import {
   ActionBuilder,
   HttpActionBuilder,
